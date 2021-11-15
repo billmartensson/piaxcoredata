88b23d755c41cf999e38161fffc37823ac2230f1
Bill Mårtensson <bill@magictechnology.se>
2021-11-15 14:06:36 +0100

    Core data

--- piaxcoredata/Persistence.swift
+++ piaxcoredata/Persistence.swift
@@ -28,10 +28,10 @@ struct PersistenceController {
         return result
     }()
 
-    let container: NSPersistentContainer
+    let container: NSPersistentCloudKitContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "piaxcoredata")
+        container = NSPersistentCloudKitContainer(name: "piaxcoredata")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
